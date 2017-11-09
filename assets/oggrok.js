393fc85cdb30883cb6ef3a301440eadc99bdec06
Orestes <oresteszoupanos@hotmail.com>
2017-11-09 17:13:03 +0000

    Add an Opus file handler!

--- assets/oggrok.js
+++ assets/oggrok.js
@@ -1,7 +1,24 @@
 // Check for the various File API support.
-var supported = "Supported APIs:"
-if (window.File) supported += "\n - File";
-if (window.FileReader) supported += "\n - FileReader";
-if (window.FileList) supported += "\n - FileList";
-if (window.Blob) supported += "\n - Blob";
-alert(supported);
+var us = "Unsupported APIs:"
+if (!window.File) us += "\n - File";
+if (!window.FileReader) us += "\n - FileReader";
+if (!window.FileList) us += "\n - FileList";
+if (!window.Blob) us += "\n - Blob";
+if (us != "Unsupported APIs:") alert(us);
+
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push(
+	  '<li><strong>', f.name, '</strong> (', f.type || 'n/a', ') - '
+	  ,f.size.toLocaleString(), ' bytes</li>'
+	  );
+  }
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+document.getElementById('files').addEventListener('load', handleFileSelect, false);
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
