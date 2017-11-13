1a7a9c74457ae4ae71881e9c245a9a05c2cabc70
Orestes <oresteszoupanos@hotmail.com>
2017-11-13 00:45:21 +0000

    Add bytes 0-50

--- assets/oggrok.js
+++ assets/oggrok.js
@@ -11,23 +11,36 @@ if(us != "Unsupported APIs:"){alert(us);}
 
 function handleFile(evnt) {
   var files = evnt.target.files;
+  document.getElementById('list').innerHTML = '';
   // files is an array of File objects. List some properties
   console.log("# of files selected: ",files.length);
 
   // cycle through all files and add their details to the output tags
   var output = [];
-  for(var i=0,f;f=files[i];i++){
+  var readers = [];
+  for(var i=0,f; f=files[i]; i++){
     // save file to a URL and feed to audio tag source
     var fileURL = window.URL.createObjectURL(f);
-    output.push('<li><audio id="audio" controls src="',fileURL,'"></audio> - <strong>', f.name, '</strong> (', f.type || 'n/a', ') - ', f.size.toLocaleString(), ' bytes.</li>');
+	
+    document.getElementById('list').innerHTML += ['<li><audio id="audio',i,'" controls src="',fileURL,'"></audio>'
+	  ,' - <strong>', f.name, '</strong> (', f.type || 'n/a', ') - ', f.size.toLocaleString(), ' bytes.'
+	  ,'<div id="header',i,'"></div></li>'].join('');
+
+    var file = files[i];
+    var start = parseInt(0) || 0;
+    var stop = parseInt(50) || file.size - 1;
+
+    readers[i] = new FileReader();
+
+    // If we use onloadend, we need to check the readyState.
+    readers[i].onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        document.getElementById(['header',i-1].join('')).innerHTML = ['Bytes ', start, '-', stop,': ',evt.target.result].join('');
+      }
+    };
+
+    readers[i].readAsBinaryString(file.slice(start, stop));
   }
-  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-
-  var blob = files[0].slice(0,3);
-  console.log(toString(blob));
-
-  var blob2 = fileURL.slice(0,3);
-  console.log(blob2);
 }
 
 document.getElementById('files').addEventListener('load', handleFile, false);
