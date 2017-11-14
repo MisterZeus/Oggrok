0d79a55a558320d5f07035a645ea7d1356fbf9ee
Orestes <oresteszoupanos@hotmail.com>
2017-11-14 17:16:44 +0000

    Fix quote breakage

--- assets/oggrok.js
+++ assets/oggrok.js
@@ -1,4 +1,4 @@
-// check if this browser supports various APIs we need
+//check if this browser supports various APIs we need
 var us = "Unsupported APIs:";
 
 if(!window.Blob){us += "\n - Blob";}
@@ -9,21 +9,21 @@ if(!window.URL){us += "\n - URL";}
 
 if(us != "Unsupported APIs:"){alert(us);}
 
-function handleFile(evnt) {
-  var files = evnt.target.files;
-  document.getElementById("list").innerHTML = "";
-  // files is an array of File objects. List some properties
+function loadFiles(evnt) {
+  document.getElementById("list").innerHTML = ""; //clear previously loaded files
+
+  var files = evnt.target.files; //files is an array of File objects. List some properties
   console.log("# of files selected: ",files.length);
 
-  // cycle through all files and add their details to the output tags
+  //cycle through all files and add their details to the output tags
   var readers = [];
   for(var i=0,f; f=files[i]; i++){
-    // save file to a URL and feed to audio tag source
+    //save file to a URL and feed to audio tag source
     var fileURL = window.URL.createObjectURL(f);
 
-    document.getElementById("list").innerHTML += ["<li><audio id="audio",i,"" controls src="",fileURL,""></audio>"
-      ," - <strong>", f.name, "</strong> (", f.type || "n/a", ") - ", f.size.toLocaleString(), " bytes."
-      ,"<div id="header",i,""></div></li>"].join("");
+    document.getElementById("list").innerHTML += ['<li><audio id="audio',i,'" controls src="',fileURL,'"></audio>'
+      ,' - <strong>', f.name, '</strong> (', f.type || "n/a", ') - ', f.size.toLocaleString(), ' bytes.'
+      ,'<div id="header',i,'"></div></li>'].join('');
 
     if (files.length==1) {
       var file = files[0]
@@ -32,11 +32,11 @@ function handleFile(evnt) {
 
       readers[0] = new FileReader();
 
-      // If we use onloadend, we need to check the readyState.
+      //If we use onloadend, we need to check the readyState.
       readers[0].onloadend = function(evt) {
-        if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        if (evt.target.readyState == FileReader.DONE) { //DONE == 2
           byteCode = new Uint8Array();
-          bytecode = evt.target.result.replace(/OggS/g, "</pre></li><li><pre>OggS");
+          bytecode = evt.target.result//.replace(/OggS/g, "</pre></li><li><pre>OggS");
           document.getElementById(["header",0].join("")).innerHTML = ["Bytes ", start.toLocaleString(), " to ", stop.toLocaleString(),":\n<ol>",byteCode.toString(),"</pre></li></ol>"].join("");
         }
       };
@@ -46,5 +46,4 @@ function handleFile(evnt) {
   }
 }
 
-document.getElementById("files").addEventListener("load", handleFile, false);
-document.getElementById("files").addEventListener("change", handleFile, false);
+document.getElementById("files").addEventListener("change", loadFiles, false);
