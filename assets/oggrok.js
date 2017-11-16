702e4103840c62ac600405c5e9bbc67ba794b50c
Orestes <oresteszoupanos@hotmail.com>
2017-11-16 16:56:16 +0000

    Ogg packets in hex!

--- assets/oggrok.js
+++ assets/oggrok.js
@@ -1,62 +1,90 @@
+"use strict";
+
 //check if this browser supports various APIs we need
+
 var us = "Unsupported APIs:";
 
-if(!window.Blob){us += "\n - Blob";}
-if(!window.File){us += "\n - File";}
-if(!window.FileList){us += "\n - FileList";}
-if(!window.FileReader){us += "\n - FileReader";}
-if(!window.URL){us += "\n - URL";}
+if (!window.Blob) {
+  us += "\n - Blob";
+}
+if (!window.File) {
+  us += "\n - File";
+}
+if (!window.FileList) {
+  us += "\n - FileList";
+}
+if (!window.FileReader) {
+  us += "\n - FileReader";
+}
+if (!window.URL) {
+  us += "\n - URL";
+}
 
-if(us != "Unsupported APIs:"){alert(us);}
+if (us != "Unsupported APIs:") {
+  alert(us);
+}
+
+function Uint8ArrayToHex(buf) {
+  var hexEncodeArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  var arr = new Uint8Array(buf); //change our file object into a list of 8-bit integers
+  var ret = '';
+  for (var i = 0; i < arr.byteLength; i++) {
+    var code = arr[i];
+    ret += hexEncodeArray[code >>> 4]; //change the 4 Most Significant Bits into hex
+    ret += hexEncodeArray[code & 0x0F]; //change the 4 Less Significant Bits into hex
+    ret += " ";
+  }
+  return ret.replace(/4F 67 67 53/g, "</li><li>OggS").replace(/4F 70 75 73 48 65 61 64/g, "</li><li>OpusHead");
+}
 
 function loadFiles(evnt) {
   document.getElementById("list").innerHTML = ""; //clear previously loaded files
 
-  const files = evnt.target.files; //files is an array of File objects. List some properties
-  console.log("# of files selected: ",files.length);
+  var files = evnt.target.files; //files is an array of File objects. List some properties
+  console.log("# of files selected: ", files.length);
 
   //cycle through all files and add their details to the output tags
   var readers = [];
-  for(var i=0,f; f=files[i]; i++){
-    //save file to a URL and feed to audio tag source
-    const fileURL = window.URL.createObjectURL(f);
+  for (var i = 0; i < files.length; i++) {
+    var f = files[i];
+    var fileURL = window.URL.createObjectURL(f); //save file to a URL, to feed to audio tag source
 
-    document.getElementById("list").innerHTML += ['<li><audio id="audio',i,'" controls src="',fileURL,'"></audio>'
-      ,' - <strong>', f.name, '</strong> (', f.type || "n/a", ') - ', f.size.toLocaleString(), ' bytes.'
-      ,'<div id="header',i,'"></div></li>'].join('');
+    document.getElementById("list").innerHTML += ['<li><audio id="audio', i, '" controls src="', fileURL, '"></audio>', ' - <strong>', f.name, '</strong> (', f.type || "n/a", ') - ', f.size.toLocaleString(), ' bytes.', '<div id="header', i, '"></div></li>'].join('');
 
-    if (files.length==1) {
-      const file = files[0]
-        ,start = parseInt(0) || 0
-        ,stop = parseInt("eggs") || file.size - 1;
+    if (files.length == 1) {
+      (function () {
+        var file = files[0],
+            start = parseInt(0) || 0,
+            stop = parseInt("eggs") || file.size - 1;
 
-      readers[0] = new FileReader();
+        readers[0] = new FileReader();
 
-      //If we use onloadend, we need to check the readyState.
-      readers[0].onloadend = function(evt) {
-        if (evt.target.readyState == FileReader.DONE) { //DONE == 2
-          var byteCode = new Uint8Array();
-          byteCode = evt.target.result//.replace(/OggS/g, "</pre></li><li><pre>OggS");
-          document.getElementById(["header",0].join("")).innerHTML = ["Bytes ", start.toLocaleString(), " to ", stop.toLocaleString(),":\n<ol>",byteCode.toString(),"</pre></li></ol>"].join("");
-        }
-      };
+        readers[0].onloadend = function (evt) {
+          //since we are using onloadend, we need to check the readyState === DONE
+          if (evt.target.readyState === FileReader.DONE) {
+            var byteCode = new Uint8Array();
+            byteCode = evt.target.result;
+            document.getElementById(["header", 0].join("")).innerHTML = ["Bytes ", start.toLocaleString(), " to ", stop.toLocaleString(), ":\n<ol>", Uint8ArrayToHex(byteCode).toString(), "</pre></li></ol>"].join("");
+          }
+        };
 
-      readers[i].readAsArrayBuffer(file.slice(start, stop));
+        readers[i].readAsArrayBuffer(file.slice(start, stop));
+      })();
     }
   }
 }
 
 function readmultifiles(e) {
-  const files = e.currentTarget.files;
-  Object.keys(files).forEach(i => {
-    const file = files[i];
-    const reader = new FileReader();
-    reader.onload = (e) => {
+  var files = e.currentTarget.files;
+  Object.keys(files).forEach(function (i) {
+    var file = files[i];
+    var reader = new FileReader();
+    reader.onload = function (e) {
       //server call for uploading or reading the files one by one
       //by using 'reader.result' or 'file'
-    }
+    };
     reader.readAsBinaryString(file);
-  })
+  });
 };
 
 document.getElementById("files").addEventListener("change", loadFiles, false);
